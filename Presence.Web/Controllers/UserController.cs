5d316ff80d0a90b2dd6d49739cf63d11113e04bc
Nikola Ivanov <nikola4421@gmail.com>
2018-05-06 15:16:35 +0300

    Adding register endpoint

---
+++ Presence.Web/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Presence.Data.Models;
+using Presence.Web.Models.User;
+
+namespace Presence.Web.Controllers
+{
+    [Route("api/User")]
+    public class UserController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var user = new User
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await this.userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return this.Ok();
+            }
+
+            this.AddErrors(result);
+            return this.BadRequest(this.ModelState);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                if (error.Code == "DuplicateUserName")
+                {
+                    continue;
+                }
+
+                if (error.Code.Contains("Email"))
+                {
+                    this.ModelState.AddModelError("Email", error.Description);
+                }
+                else if (error.Code.Contains("Password"))
+                {
+                    this.ModelState.AddModelError("Password", error.Description);
+                }
+                else
+                {
+                    this.ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+        }
+    }
+}
