334531703722989eeaeba1426148cad34ed851e5
Nikola Ivanov <nikola4421@gmail.com>
2018-05-09 21:29:14 +0300

    Adding basic routing to client

--- Presence.Client/src/app/app.routing.module.ts
+++ Presence.Client/src/app/app.routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+
+const routes: Routes = [
+	// Have a default home component which to be served as a landing page
+	// { path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class AppRoutingModule { }
